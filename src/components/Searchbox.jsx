f0dc6f7d8bc4809ce7409c5aadf4ca6e87ff5dfc
vishal modanwal <vk0764394@gmail.com>
2024-06-27 18:10:50 +0530

    fiter-set for SearchBar

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 
 
 function Searchbox() {
+  const [jobCriteria , setJobCriteria] =useState({
+    title:"",
+    experience: "",
+    location:"",
+    type:""
+  });
+   const handleChange =(e)=>{
+      setJobCriteria((prevState)=>({
+        ...prevState,
+        [e.target.name] : e.target.value
+      }))
+    }
+ console.log(jobCriteria);
+
   return (
     <div className='flex'>
-      <select className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl scroll-smooth-4'>
+      <select onChange={ handleChange } name='title' value={jobCriteria.title} className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl '>
         <option value="" disabled hidden selected>Job Role</option>
         <option value="Frontend-developer">Frontend-developer</option>
         <option value="Backend-developer">Backend-developer</option>
@@ -13,19 +28,19 @@ function Searchbox() {
         <option value="Java-developer">Java-developer</option>
         <option value="Bussines Analist">Bussiness Analist</option>
       </select>
-      <select className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl'>
+      <select onChange={ handleChange } name='type' value={jobCriteria.type} className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl'>
         <option value="" disabled hidden selected>Job Type</option>
         <option value="Full-Time">Full-Time</option>
         <option value="Part-Time">Part-Time</option>
         <option value="Contract">Contract</option>
       </select>
-      <select className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl'>
+      <select onChange={ handleChange }  name='location' value={jobCriteria.location}  className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl'>}  className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl'>
         <option value="" disabled hidden selected>Location</option>
         <option value="In-Office">In-Office</option>
         <option value="Remote">Remote</option>
         <option value="Hybrid">Hybrid</option>
       </select>
-      <select className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl'>
+      <select onChange={ handleChange }  name='experience' value={jobCriteria.experience} className='w-64 py-3 mx-4 my-5 pl-4 font-bold bg-zinc-200 rounded-xl'>
         <option value="" disabled hidden selected>Experience</option>
         <option value="Fresherr">Fresher</option>
         <option value="Junior Level">Junior Level</option>
